fe397d530dc775f05f78af70969a18da14ce5372
sahiladhikari39 <sahiladhi39@gmail.com>
2025-10-27 11:53:05 +0545

    learned about objects and its properties

---
+++ day5/objects2.js
@@ -0,0 +1,44 @@
+// // const championsLeague = new object() //singleton object
+
+// const superLeague = {} //non-singleton object
+
+// superLeague.name = "Nepal Super League";
+// superLeague.id = "12321";
+// console.log(superLeague);
+
+
+
+// //combining two objects
+// const obj1 = {
+//     1 : 'a',
+//     2 : 'b'
+// }
+
+// const obj2 = {
+//     3 : 'c',
+//     4 : 'd'
+// }
+
+// const obj3 = Object.assign(obj1, obj2);
+// const obj4 = Object.assign({}, obj1, obj2);
+
+// console.log(obj3);
+// console.log(obj4);
+
+
+
+
+//most used method for combining two objects
+const obj1 = {
+    name : "Messi",
+    club : "Miami",
+}
+
+const obj2 = {
+    name : "Squarez",
+    club : "Miami"
+}
+
+const footballers = {...obj1, ...obj2} //... bhneko spread ho hae
+console.log(footballers);
+
