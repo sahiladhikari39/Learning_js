4ce3e1129d066e836e7df255792368693c3ab52b
sahiladhikari39 <sahiladhi39@gmail.com>
2025-10-30 10:34:55 +0545

    learned selection of element, html, css, event listener

--- advance/day1/script.js
+++ advance/day1/script.js
@@ -24,3 +24,18 @@ btn.addEventListener("click", function(){
     btn.style.color = 'white';
     btn.style.backgroundColor = '#252525'
 })
+
+
+var bulbOn = 0; 
+var btn2 = document.querySelector("#bulb-btn");
+var bulb = document.querySelector("#bulb");
+btn2.addEventListener("click", function(){
+    if(bulbOn == 0){
+        bulb.style.backgroundColor = "orange";
+        bulbOn = 1;
+    }else{
+        bulb.style.backgroundColor = "white";
+        bulbOn = 0;
+    }
+    
+})
